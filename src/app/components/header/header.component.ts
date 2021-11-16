b6bcc9184a06e4eb6462d0b29f335e24cd0e345b
GabyC116 <>
2021-11-15 22:24:22 -0400

    BB-Header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public userName = "Juan";
+  public userRole = "Administrador";
+  public campanyName = "Mi Empresa";
+  faUser = faUser;
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
   }
 
+  isInformationRoute() {
+    var homeRoute;
+    if (this.router.url == '/') {
+      homeRoute = false;
+    }
+    else {
+      homeRoute = true;
+    }
+    return homeRoute;
+  }
 }
