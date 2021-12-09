7561a890b4b0c18cfdd9401f0055ca5de7750db0
Jeyson <88443039+Jasson5@users.noreply.github.com>
2021-12-09 17:52:50 -0400

    add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  //BACK_END_HOST: 'https://supportsystemtis.azurewebsites.net/api/',
-  BACK_END_HOST: 'https://localhost:44341/api/',
+  BACK_END_HOST: 'https://supportsystemtis.azurewebsites.net/api/',
+  //BACK_END_HOST: 'https://localhost:44341/api/',
   ACCES_KEY_ID: 'PLWIAX4TEUVTUP43VQRG',
   SECRET_ACCESS_KEY: 'rKK0JTmHIx9jZtBBYPvoeWmUqvpcfiI82ZwyfVfF7uc',
   REGION: 'nyc3',
